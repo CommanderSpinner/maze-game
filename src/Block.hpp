30fc28c841a74a6a2c2b5957236f4195b89ed0a5
CommanderSpinner <klaus.kutulu@gmail.com>
2026-06-01 20:06:32 +0200

    written constructor for block class

--- src/Block.hpp
+++ src/Block.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
 #include "Entity.hpp"
+#include "Global.hpp"
 
 class Block : public Entity {
-    
+    Block(sf::Vector2 position) : Entity(Global::defaultTexture) {
+        setTexture(Global::blockTexture);
+        sprite.setPosition(position);
+    }
 };
--- src/Global.hpp
+++ src/Global.hpp
@@ -6,4 +6,5 @@ namespace Global
 {
     inline std::string playerTexturePath = "assets/Player.png";
     inline std::string defaultTexture = "assets/Default.png";
+    inline std::string blockTexture = "assets/Block.png";
 }
