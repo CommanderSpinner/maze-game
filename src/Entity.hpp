53471fae5d70a2920609c30b3aea312f00022e59
CommanderSpinner <klaus.kutulu@gmail.com>
2026-06-01 13:55:58 +0200

    written some methods for entitty

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -4,12 +4,33 @@
 
 
 class Entity {
+protected:
+    sf::Sprite sprite;
+    sf::Vector2f velocity;
 public:
-    int cordsX = 0, cordsY = 0;
-    sf::RectangleShape box({50.f, 50.f});
 
     virtual ~Entity() = default;
+    Entity() : velocity({0.f, 0.f}) {}
 
-    virtual void update(float dt) {}
-    virtual void draw(sf::RenderWindow& window) {}
+    virtual void update(float deltaTime) {}
+    
+    virtual void draw(sf::RenderWindow& window) {
+        window.draw(sprite);
+    }
+
+    void setPosition(const sf::Vector2f& pos) {
+        sprite.setPosition(pos);
+    }
+
+    sf::Vector2f getPosition() const {
+        return sprite.getPosition();
+    }
+
+    void setTexture(const sf::Texture& texture) {
+        sprite.setTexture(texture);
+    }
+
+    sf::FloatRect getBounds() const {
+        return sprite.getGlobalBounds();
+    }
 };
