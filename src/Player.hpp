59040a1cd78989129bc565ce891d96379a2999b5
CommanderSpinner <klaus.kutulu@gmail.com>
2026-06-01 17:52:11 +0200

    player now needs a texture when creating too

--- src/Player.hpp
+++ src/Player.hpp
@@ -6,10 +6,19 @@
 class Player :  public Entity {
 private: 
     InputManager& input;
+
+    void handleInput() {
+
+    }
 public:
-    Player(InputManager& im) : input(im) {}
+    Player(InputManager& im, std::string texturePath) : input(im) {
+        setTexture(texturePath);
+
+    }
     void update(float deltaTime) override {
         // todo: add here code for player movement based on what input manager passes
+        handleInput();
         Entity::update(deltaTime);
     }
+    
 };
