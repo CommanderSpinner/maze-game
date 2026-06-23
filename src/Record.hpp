10fd4aadfa2359625616c7a9d035606b7a3c015d
CommanderSpinner <klaus.kutulu@gmail.com>
2026-06-23 15:27:27 +0200

    written recordToEntity function

--- src/Record.hpp
+++ src/Record.hpp
@@ -3,6 +3,7 @@
 #include "EntityType.hpp"
 #include "Entity.hpp"
 #include <cstdio>
+#include <memory>
 
 struct record {
     size_t id;
@@ -21,4 +22,30 @@ inline record entityToRecord(Entity& entity) {
     r.type = entityTypeToString(entity.getType());
 
     return r;
+}
+
+inline std::unique_ptr<Entity> recordToEntity(record r, InputManager& im) {
+    std::unique_ptr<Entity> e;
+
+    // creating corresponding object type to entityType
+    switch(stringToEntityType(r.type)) {
+        case entityType::BLOCK:
+            e = std::make_unique<Block>(sf::Vector2f{0.f, 0.f});
+            break;
+        case entityType::PLAYER:
+            e = std::make_unique<Player>(im);
+        case entityType::ENEMY:
+            e = std::make_unique<Enemy>(sf::Vector2f{0.f, 0.f});
+            break;
+        case entityType::ITEM:
+            break;
+    }
+
+    // setting other attributes
+
+    e->setID(r.id);
+    e->setHealth(r.health);
+    e->setPosition(sf::Vector2f{r.x, r.y});
+
+    return e;
 }
